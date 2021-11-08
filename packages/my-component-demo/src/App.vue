82f680d22b5a85a2924c415792748c7deab56d84
andreacampaci <andreacampaci@hotmail.it>
2021-11-08 13:25:49 +0100

    update demo components

--- packages/my-component-demo/src/App.vue
+++ packages/my-component-demo/src/App.vue
@@ -1,27 +1,29 @@
 <template>
-    <div class="text-center">
-      <img src="./assets/logo.jpeg" alt="logo.jpeg">
-      <h1>Hello <strong>Coder</strong>!</h1>
-    </div>
+  <div class="text-center">
+    <img src="./assets/logo.jpeg" alt="logo.jpeg">
+    <h1>Hello <strong>Coder</strong>!</h1>
+    <!--<h1>Hello <strong>{{coderName}}</strong>!</h1>-->
+  </div>
 </template>
 
 <script lang="ts">
-  import { Component, Vue, Emit, Prop} from 'vue-property-decorator';
+import {Component, Vue, Emit, Prop} from 'vue-property-decorator';
 
-  @Component
-  export default class App extends Vue {
-  }
+@Component
+export default class App extends Vue {
+  // @Prop({ default: () => "Unknow"}) public coderName!: string;
+}
 </script>
 
 <style lang="scss">
-  @import 'bootstrap/scss/bootstrap';
-  @import 'bootstrap-vue/src/index.scss';
+@import 'bootstrap/scss/bootstrap';
+@import 'bootstrap-vue/src/index.scss';
 </style>
 
 <style lang="scss" scoped>
- h1 {
-   strong {
-     color: orangered;
-   }
- }
+h1 {
+  strong {
+    color: orangered;
+  }
+}
 </style>
--- packages/my-component-demo/tests/unit/example.spec.ts
+++ packages/my-component-demo/tests/unit/example.spec.ts
@@ -1,12 +1,12 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import App from '@/App.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+describe('App.vue', () => {
+  // it('should be a text in a component', () => {
+  //   const coderName = 'Andrea';
+  //   const wrapper = shallowMount(App, {
+  //     propsData: { coderName },
+  //   });
+  //   expect(wrapper.text()).toMatch(coderName);
+  // });
 });
